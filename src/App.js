1530ff98c69a4fe3dbf4ab324884ab47009aedaa
vineeshkalarickal <vineesh.kalarickal@gmail.com>
2024-10-10 12:37:58 +0530

    added index page - home

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Navigation />} >
-        <Route index={true} element={<Home />} />
+        <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
       </Route>
     </Routes>
