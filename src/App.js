56d32442a81e5639ead8b2fe90a854d4870ff67c
vineeshkalarickal <vineesh.kalarickal@gmail.com>
2024-10-10 12:37:36 +0530

    added Outlet for sub route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
 import Home from './routes/home/home.components';
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>This is the Navigation bar</h1>
+      </div>
+      <Outlet />
+    </div>
+  )
+}
+const Shop = () => {
+  return <h1>This is to test my Shop Page</h1>
+}
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/' element={<Navigation />} >
+        <Route index={true} element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
     </Routes>
   )
 };
